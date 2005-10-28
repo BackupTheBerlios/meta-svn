a5c628aa0b7dfc5b62b8015b97debe9760784b99
chrissy <chrissy@3a1844c4-d2eb-0310-aa10-b5cd706e6782>
2005-10-28 20:21:00 +0000

    bugfix parser, added test

--- Meta/Meta.cs
+++ Meta/Meta.cs
@@ -3927,7 +3927,7 @@ namespace Meta
 		}
 		private bool LookAny(char[] any)
 		{
-			return !LookExcept(any);
+			return Look().ToString().IndexOfAny(any)!=-1;
 		}
 		public char[] integerChars=new char[] {'0','1','2','3','4','5','6','7','8','9'};
 		public char[] firstIntegerChars=new char[] {'1','2','3','4','5','6','7','8','9'};
