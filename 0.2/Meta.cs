95ad8a6f3c288fd53979a8d6ec91f58602401787
staudenmeyer <staudenmeyer@3a1844c4-d2eb-0310-aa10-b5cd706e6782>
2006-05-31 14:38:24 +0000

    infix statements working

--- 0.2/Meta.cs
+++ 0.2/Meta.cs
@@ -7653,7 +7653,14 @@ namespace Meta
 			//        return Run(@"C:\Meta\0.2\Test\newBasicTest.meta", new StrategyMap(1, "first arg", 2, "second=arg"));
 			//    }
 			//}
-
+			public class Library : Test
+			{
+				public override object GetResult(out int level)
+				{
+					level = 2;
+					return Run(@"C:\Meta\0.2\Test\libraryTest.meta", new StrategyMap(1, "first arg", 2, "second=arg"));
+				}
+			}
 			public class Serialization : Test
 			{
 				public override object GetResult(out int level)
@@ -7670,14 +7677,7 @@ namespace Meta
 					return Run(@"C:\Meta\0.2\Test\basicTest.meta", new StrategyMap(1, "first arg", 2, "second=arg"));
 				}
 			}
-			public class Library : Test
-			{
-				public override object GetResult(out int level)
-				{
-					level = 2;
-					return Run(@"C:\Meta\0.2\Test\libraryTest.meta", new StrategyMap(1, "first arg", 2, "second=arg"));
-				}
-			}
+
 			//public class Extents : Test
 			//{
 			//    public override object GetResult(out int level)
