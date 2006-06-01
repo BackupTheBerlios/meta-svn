d0bb68b9db80ab8b28080d95effc73af4cacada1
staudenmeyer <staudenmeyer@3a1844c4-d2eb-0310-aa10-b5cd706e6782>
2006-06-01 21:39:51 +0000

    basic test working without points

--- 0.2/Meta.cs
+++ 0.2/Meta.cs
@@ -6185,14 +6185,7 @@ new Assignment(
 			//        return Run(@"C:\Meta\0.2\Test\newBasicTest.meta", new StrategyMap(1, "first arg", 2, "second=arg"));
 			//    }
 			//}
-			public class Library : Test
-			{
-				public override object GetResult(out int level)
-				{
-					level = 2;
-					return Run(@"C:\Meta\0.2\Test\libraryTest.meta", new StrategyMap(1, "first arg", 2, "second=arg"));
-				}
-			}
+
 			public class Serialization : Test
 			{
 				public override object GetResult(out int level)
@@ -6209,7 +6202,14 @@ new Assignment(
 					return Run(@"C:\Meta\0.2\Test\basicTest.meta", new StrategyMap(1, "first arg", 2, "second=arg"));
 				}
 			}
-
+			public class Library : Test
+			{
+				public override object GetResult(out int level)
+				{
+					level = 2;
+					return Run(@"C:\Meta\0.2\Test\libraryTest.meta", new StrategyMap(1, "first arg", 2, "second=arg"));
+				}
+			}
 			//public class Extents : Test
 			//{
 			//    public override object GetResult(out int level)
