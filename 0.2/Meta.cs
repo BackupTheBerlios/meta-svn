f5f916ab7f077cbf930645bab7d62213c0c3566f
staudenmeyer <staudenmeyer@3a1844c4-d2eb-0310-aa10-b5cd706e6782>
2006-10-04 13:31:51 +0000

    removed comments

--- 0.2/Meta.cs
+++ 0.2/Meta.cs
@@ -213,7 +213,6 @@ namespace Meta
 						if (method.method.IsStatic  || method.method is ConstructorInfo)
 						{
 							arguments=new List<object>();
-							//List<object> arguments = new List<object>();
 							for (int i = 0; i < method.parameters.Length; i++)
 							{
 								Map arg = calls[i+1].EvaluateStructure();
@@ -221,7 +220,6 @@ namespace Meta
 								{
 									m = null;
 									return false;
-									//return null;
 								}
 								else
 								{
@@ -230,107 +228,14 @@ namespace Meta
 							}
 							m = method.method;
 							return true;
-							//Map result = (Map)method.method.Invoke(null, arguments.ToArray());
-							//result.IsConstant = false;
-							//return result;
-							//if (method.method.GetCustomAttributes(typeof(CompilableAttribute), false).Length != 0)
-							//{
-							//    List<object> arguments = new List<object>();
-							//    for (int i = 1; i < calls.Count; i++)
-							//    {
-							//        Map arg = calls[i].EvaluateStructure();
-							//        if (arg == null)
-							//        {
-							//            return null;
-							//        }
-							//        else
-							//        {
-							//            arguments.Add(Transform.ToDotNet(arg.Copy(), method.parameters[i - 1].ParameterType));
-							//        }
-							//    }
-							//Map result = (Map)method.method.Invoke(null, arguments.ToArray());
-							//result.IsConstant = false;
-							//return result;
-							//}
 						}
-						//else if (method.method is ConstructorInfo)
-						//{
-						//    //Dictionary<Map, Member> type = ObjectMap.cache.GetMembers(method.method.DeclaringType);
-						//    //Structure result = new Structure();
-						//    //foreach (Map key in type.Keys)
-						//    //{
-						//    //    result[key] = new Unknown();
-						//    //}
-						//    //return result;
-						//}
 					}
 				}
 			}
 			arguments = null;
 			m = null;
 			return false;
-			//return null;
 		}
-		//public override Map StructureImplementation()
-		//{
-		//    Map first = calls[0].EvaluateStructure();
-		//    if (first != null && first.IsConstant)
-		//    {
-		//        Method method;
-		//        if (first.Strategy is TypeMap)
-		//        {
-		//            method = (Method)((TypeMap)first.Strategy).Constructor.Strategy;
-		//        }
-		//        else if (first.Strategy is Method)
-		//        {
-		//            method = (Method)first.Strategy;
-		//        }
-		//        else
-		//        {
-		//            method = null;
-		//        }
-		//        if(method!=null)
-		//        {
-		//            //Method method = (Method)first.Strategy;
-		//            if (method.parameters.Length == calls.Count - 1 || (calls.Count==2 && method.parameters.Length==0))
-		//            {
-		//                if (method.method.IsStatic)
-		//                {
-		//                    if (method.method.GetCustomAttributes(typeof(CompilableAttribute), false).Length != 0)
-		//                    {
-		//                        List<object> arguments = new List<object>();
-		//                        for (int i = 1; i < calls.Count; i++)
-		//                        {
-		//                            Map arg = calls[i].EvaluateStructure();
-		//                            if (arg == null)
-		//                            {
-		//                                return null;
-		//                            }
-		//                            else
-		//                            {
-		//                                arguments.Add(Transform.ToDotNet(arg.Copy(), method.parameters[i - 1].ParameterType));
-		//                            }
-		//                        }
-		//                        Map result = (Map)method.method.Invoke(null, arguments.ToArray());
-		//                        result.IsConstant = false;
-		//                        return result;
-		//                    }
-		//                }
-		//                else if (method.method is ConstructorInfo)
-		//                {
-		//                    Dictionary<Map, Member> type = ObjectMap.cache.GetMembers(method.method.DeclaringType);
-		//                    Structure result = new Structure();
-		//                    foreach (Map key in type.Keys)
-		//                    {
-		//                        result[key] = new Unknown();
-		//                    }
-		//                    return result;
-		//                }
-		//            }
-		//        }
-		//    }
-		//    return null;
-		//}
 		public override Compiled CompileImplementation(Expression parent)
 		{
 			return new CompiledCall(calls.ConvertAll<Compiled>(delegate(Expression e)
